66d4f308b4de4754a9f131644cc9d3546581e585
Michał Wilk <dev2theworld@gmail.com>
2021-07-02 15:13:15 +0200

    JS - functions createCards() & createCard() added & called

--- js/main.js
+++ js/main.js
@@ -31,3 +31,38 @@ const cardsData = [
   answer: 'The Last answer...'
 }
 ];
+
+// 1d - Create all Cards
+function createCards() {
+  cardsData.forEach((data, index) => createCard(data, index));
+}
+
+// 1e - Create sigle Card in DOM
+function createCard(data, index) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  // 1f - Add active class to 1st card
+  if(index === 0) {
+    card.classList.add('active');
+  }
+
+  // 1g - filling HTML
+  card.innerHTML = `
+  <div class="inner-card">
+    <div class="inner-card-front">
+      <p>${data.question}</p>
+    </div>
+    <div class="inner-card-back">
+      <p>${data.answer}</p>
+    </div>
+  </div>
+  `;
+
+  // 1h - Add to DOM Cards Element
+  cardsElement.push(card);
+
+  cardsContainer.appendChild(card);
+}
+
+createCards();
